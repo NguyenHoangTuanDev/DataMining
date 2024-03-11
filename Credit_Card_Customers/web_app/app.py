da0a769ba04520d9aa6ecfe7e17808f5faacc717
NguyenHoangTuanDev <149009982+NguyenHoangTuanDev@users.noreply.github.com>
2024-03-11 13:30:33 +0700

    Update app.py

--- Credit_Card_Customers/web_app/app.py
+++ Credit_Card_Customers/web_app/app.py
@@ -14,7 +14,7 @@ col1, col2 = st.columns(2)
 
 with col1:
 
-    Customer_Age = st.slider('Customer_Age:', value=45, min_value=1, max_value=120)
+    Customer_Age = st.slider('Customer_Age:', value= None, min_value=1, max_value=120)
 
     Gender = st.radio('Gender:', options=['M (Male)','F (Female)'])
 
@@ -24,34 +24,34 @@ with col1:
 
     Marital_Status = st.selectbox('Marital_Status:', index=0, options=['Married', 'Single', 'Divorced', 'Unknown'])
 
-    Income_Category = st.select_slider('Income_Category', value='$60K - $80K', options=['Less than $40K', '$40K - $60K', '$60K - $80K', '$80K - $120K', '$120K +', 'Unknown'])
+    Income_Category = st.select_slider('Income_Category', value= None, options=['Less than $40K', '$40K - $60K', '$60K - $80K', '$80K - $120K', '$120K +', 'Unknown'])
 
     Card_Category = st.selectbox('Card_Category', index=0, options=['Blue', 'Gold', 'Silver', 'Platinum'])
 
-    Months_on_book = st.number_input('Months_on_book', value=39, min_value=0, max_value=100)
+    Months_on_book = st.number_input('Months_on_book', value= None, min_value=0, max_value=100)
 
-    Total_Relationship_Count = st.number_input('Total_Relationship_Count', value=5, min_value=0, max_value=10)
+    Total_Relationship_Count = st.number_input('Total_Relationship_Count', value=None, min_value=0, max_value=10)
 
 with col2:
-    Months_Inactive_12_mon = st.number_input('Months_Inactive_12_mon', value=1, min_value=0, max_value=12)
+    Months_Inactive_12_mon = st.number_input('Months_Inactive_12_mon', value=None, min_value=0, max_value=12)
 
-    Contacts_Count_12_mon = st.number_input('Contacts_Count_12_mon', value=3, min_value=0, max_value=10)
+    Contacts_Count_12_mon = st.number_input('Contacts_Count_12_mon', value=None, min_value=0, max_value=10)
 
-    Credit_Limit = st.number_input('Credit_Limit', value=12691, min_value=0)
+    Credit_Limit = st.number_input('Credit_Limit', value=None, min_value=0)
 
-    Total_Revolving_Bal = st.number_input('Total_Revolving_Bal', value=777, min_value=0)
+    Total_Revolving_Bal = st.number_input('Total_Revolving_Bal', value=None, min_value=0)
 
-    Avg_Open_To_Buy = st.number_input('Avg_Open_To_Buy', value=11914, min_value=0)
+    Avg_Open_To_Buy = st.number_input('Avg_Open_To_Buy', value=None, min_value=0)
 
-    Total_Amt_Chng_Q4_Q1 = st.number_input('Total_Amt_Chng_Q4_Q1', value=1.335, min_value=0.0)
+    Total_Amt_Chng_Q4_Q1 = st.number_input('Total_Amt_Chng_Q4_Q1', value=None, min_value=0.0)
 
-    Total_Trans_Amt = st.number_input('Total_Trans_Amt', value=1144.5, min_value=0.01)
+    Total_Trans_Amt = st.number_input('Total_Trans_Amt', value=None, min_value=0.01)
 
-    Total_Trans_Ct = st.number_input('Total_Trans_Ct', value=42, min_value=0)
+    Total_Trans_Ct = st.number_input('Total_Trans_Ct', value=None, min_value=0)
 
-    Total_Ct_Chng_Q4_Q1 = st.number_input('Total_Ct_Chng_Q4_Q1', value=1.625, min_value=0.0)
+    Total_Ct_Chng_Q4_Q1 = st.number_input('Total_Ct_Chng_Q4_Q1', value=None, min_value=0.0)
 
-    Avg_Utilization_Ratio = st.number_input('Avg_Utilization_Ratio', value=0.061, min_value=0.0)
+    Avg_Utilization_Ratio = st.number_input('Avg_Utilization_Ratio', value=None, min_value=0.0)
 
 
 def predict_Attrition_Flag(
